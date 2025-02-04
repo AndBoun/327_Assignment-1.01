73ef1ee53bcbc387223cc319081a0d3d4a64290d
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-02-04 14:37:22 -0600

    Define global variable constants

--- dungeon_generator.c
+++ dungeon_generator.c
@@ -1,3 +1,24 @@
 //
 // Created by Andrew Boun on 2/6/25.
 //
+
+#define ROCK " "
+
+#define FLOOR "."
+#define CORRIDOR "#"
+#define WALL "#"
+
+#define DOWN_STAIRS ">" // stairs down
+#define UP_STAIRS "<" // stairs up
+
+#define HORIZONTAL_BORDER "-"
+#define VERTICAL_BORDER "|" // vertical border
+
+#define DUNGEON_WIDTH 80
+#define DUNGEON_HEIGHT 21
+
+#define MIN_ROOMS 6
+
+#define MIN_ROOM_WIDTH 4
+#define MIN_ROOM_HEIGHT 3
+
